7fcb5280b410fba692ea08eda6ac24b8af1c8b39
unknown <digitone@naver.com>
2022-07-11 15:08:55 +0900

    상세페이지 데이터 추가

--- js/detail.js
+++ js/detail.js
@@ -8,72 +8,29 @@ function getData() {
   })
   .then(function(obj){
     // obj 동물데이터
-    showProducts(obj);
+    showDetail(obj);
     console.log(obj);
   });
 }
 
-
-function showProducts(obj) {
+function showDetail(obj) {
   // 현재 페이지의 URL query 파라미터(매개변수)
   const query = location.search;
   console.log(query);
   // ? URL query문을 object(변수)로 변경
   let params = new URLSearchParams(query).get('id');
-
-  // params == null 이면(시작 페이지 모든 제품 출력)
-  if(params == null || params =='all') {
-    params = null;
-  }
   console.log(params);  
 
-  // 상품 데이터 출력
-  obj.forEach(function(product, i){
-    // 카테고리 구분 wireless | 무선헤드폰 | 유선헤드폰
-    // 요청한 params와 제품카테고리명이 일치하는 데이터만 출력
-    let category = product.category;
-    let name = product.name;
-    let price = product.price;
-    let imgUrl = product.imgUrl;
-    let text = product.text;
-    console.log(category);
+  // 해당 id 제품 출력
+  let category = obj[params].category;
+  let name = obj[params].name;
+  let price = obj[params].price;
+  let imgUrl = obj[params].imgUrl;
+  let text = obj[params].text;
+  console.log('name = ', name);
+  
+  // deltail.html에 각 변수값 주입
 
-    // 카테고리 별로 보기
-    if(params == product.category) {
-      let html = `
-      <div class="product" data-id=${i}>
-        <a href="detail.html?id=${i}">
-          <img src="${imgUrl}" alt=${name}>
-          <div class="info">
-            <span class="category">${category}</span>
-            <p class="title">${name}</p>
-            <p class="price"><span>₩</span>${price}</p>
-          </div>
-        </a>
-      </div>
-      `
-      $('main .products').append(html);
-    }
-
-    // 상품 전체보기
-    if(params == null) {
-      let html = `
-      <div class="product" data-id=${i}>
-        <a href="detail.html?id=${i}">
-          <img src="${imgUrl}" alt=${name}>
-          <div class="info">
-            <span class="category">${category}</span>
-            <p class="title">${name}</p>
-            <p class="price"><span>₩</span>${price}</p>
-          </div>
-        </a>
-      </div>
-      `
-      $('main .products').append(html);
-      console.log(`i = `, i)
-    }
-
-  });
 }
 
 $(function(){
